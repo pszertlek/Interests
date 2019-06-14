5dd5525f73566e54592969612da5242d4b40aebf
aning.pan <aning.pan@broadlink.com.cn>
2019-06-14 18:52:28 +0800

    initial

---
+++ Interests/Interests/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Interests
+//
+//  Created by apple on 2019/6/14.
+//  Copyright © 2019 Pszertlek.com.cn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ Interests/InterestsTests/InterestsTests.swift
@@ -0,0 +1,34 @@
+//
+//  InterestsTests.swift
+//  InterestsTests
+//
+//  Created by apple on 2019/6/14.
+//  Copyright © 2019 Pszertlek.com.cn. All rights reserved.
+//
+
+import XCTest
+@testable import Interests
+
+class InterestsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
